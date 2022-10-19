b05d85aa1bc697b036b3b92b5106ae29e6783d9f
Gregory Maldonado <gmaldona@oswego.edu>
2022-10-19 08:16:39 -0400

    save image script

---
+++ scripts/save_image.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+if [ ! -f crypto-server.go ]
+then
+  printf "Please run script in the working directory of api/"
+  exit 1
+fi
+
+if [ ! -d docker-save ]
+then
+    mkdir docker-save
+fi
+
+ docker save crypto-server-app > ./docker-save/maldonado-crypto-server.tar
