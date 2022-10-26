8e2bb91e47eb8f724d9f33cfd1b63508caaeb5a4
Gregory Maldonado <gmaldona@oswego.edu>
2022-10-26 18:17:46 -0400

    rm old image

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -6,18 +6,23 @@ then
   exit 1
 fi
 
+
+
 while getopts 'lp:' OPTION; do
   case "$OPTION" in
     l)
       printf "Building image for local machine.\n"
+      docker image rm maldonado-crypto-server-image-local
       docker build -t maldonado-crypto-server-image-local .
       ;;
     p)
       printf "Building image for AWS linux machine.\n"
+      docker image rm maldonado-crypto-server-image
       docker buildx build --platform linux/amd64 -t maldonado-crypto-server-image .
       ;;
     ?)
       printf "Building image for AWS linux machine.\n"
+      docker image rm maldonado-crypto-server-image
       docker buildx build --platform linux/amd64 -t maldonado-crypto-server-image .
   ;;
 esac
