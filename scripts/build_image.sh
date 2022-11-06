78bd2c9f2a28f9f3555b8d31e8f3a03e4b146c75
Gregory Maldonado <gmaldona@oswego.edu>
2022-11-06 18:56:42 -0500

    updated build image script to not include hard coded name

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -6,19 +6,19 @@ then
   exit 1
 fi
 
-
+IMAGE_NAME="" # image name
 
 while getopts 'lph' OPTION; do
   case "$OPTION" in
     l)
       printf "Building image for local machine.\n"
-      docker image rm maldonado-crypto-server-image-local
-      docker build -t maldonado-crypto-server-image-local .
+      docker image rm "$IMAGE_NAME"-local
+      docker build -t "$IMAGE_NAME"-local .
       ;;
     p)
       printf "Building image for AWS linux machine.\n"
-      docker image rm maldonado-crypto-server-image
-      docker buildx build --platform linux/amd64 -t maldonado-crypto-server-image .
+      docker image rm "$IMAGE_NAME"
+      docker buildx build --platform linux/amd64 -t "$IMAGE_NAME" .
       ;;
     h)
       printf "\055l\t Build a docker image for local machine.\n"
@@ -26,8 +26,8 @@ while getopts 'lph' OPTION; do
       ;;
     ?)
       printf "Building image for AWS linux machine.\n"
-      docker image rm maldonado-crypto-server-image
-      docker buildx build --platform linux/amd64 -t maldonado-crypto-server-image .
+      docker image rm "$IMAGE_NAME"
+      docker buildx build --platform linux/amd64 -t "$IMAGE_NAME" .
   ;;
 esac
 done
